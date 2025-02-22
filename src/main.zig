e839370fb98b7d51a36a8047b790838b0b8b8e04
wembleyleach <wembley.gl@icloud.com>
2025-02-22 09:44:23 -0500

    Implement table driven test for the Token type

--- src/main.zig
+++ src/main.zig
@@ -12,10 +12,7 @@ const Token = struct {
     text: []const u8,
 
     pub fn init(kind: TokenType, text: []const u8) Token {
-        return .{
-            .kind = kind,
-            .text = text,
-        };
+        return .{ .kind = kind, .text = text };
     }
 
     pub fn fmt(self: Token, allocator: mem.Allocator) ![]const u8 {
@@ -47,8 +44,40 @@ const TokenType = enum(u8) {
 };
 
 test "token display" {
-    const token = Token{ .kind = .eof, .text = "<EOF>" };
-    const actual = try token.fmt(test_allocator);
-    defer test_allocator.free(actual);
-    try testing.expectEqualStrings("<'<EOF>', <EOF>>", actual);
+    const test_cases = [_]struct {
+        args: struct { kind: TokenType, text: []const u8 },
+        actual: []const u8,
+    }{
+        .{
+            .args = .{ .kind = .eof, .text = "<EOF>" },
+            .actual = "<'<EOF>', <EOF>>",
+        },
+        .{
+            .args = .{ .kind = .lbrack, .text = "LBRACK" },
+            .actual = "<'LBRACK', LBRACK>",
+        },
+        .{
+            .args = .{ .kind = .rbrack, .text = "RBRACK" },
+            .actual = "<'RBRACK', RBRACK>",
+        },
+        .{
+            .args = .{ .kind = .alias, .text = "ALIAS" },
+            .actual = "<'ALIAS', ALIAS>",
+        },
+        .{
+            .args = .{ .kind = .path, .text = "PATH" },
+            .actual = "<'PATH', PATH>",
+        },
+        .{
+            .args = .{ .kind = .glob, .text = "GLOB" },
+            .actual = "<'GLOB', GLOB>",
+        },
+    };
+
+    for (test_cases) |tc| {
+        const token = Token.init(tc.args.kind, tc.args.text);
+        const actual = try token.fmt(test_allocator);
+        defer test_allocator.free(actual);
+        try testing.expectEqualStrings(tc.actual, actual);
+    }
 }
