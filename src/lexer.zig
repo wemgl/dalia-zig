1fc9d8e48eb50ad6357feab5c094d704274c9b5f
wembleyleach <wembley.gl@icloud.com>
2025-02-28 08:49:35 -0500

    Fix all Lexer tests

--- src/lexer.zig
+++ src/lexer.zig
@@ -132,201 +132,201 @@ pub const Lexer = struct {
     }
 };
 
-// test "expect Lexer initialization" {
-//     const testing = std.testing;
-//     const lexer = try Lexer.init(testing.allocator, "test", 0, 't');
-//     defer lexer.deinit();
-//
-//     try testing.expectEqualStrings("test", lexer.cursor.input);
-//     try testing.expectEqual(0, lexer.cursor.pointer);
-//     try testing.expectEqual('t', lexer.cursor.current_char);
-// }
+test "expect Lexer initialization" {
+    const testing = std.testing;
+    const lexer = try Lexer.init(testing.allocator, "test", 0, 't');
+    defer lexer.deinit();
 
-// test "expect Lexer returns token names by index" {
-//     const testing = std.testing;
-//     const lexer = try Lexer.init(testing.allocator, "test", 0, 't');
-//     defer lexer.deinit();
-//
-//     const test_cases = [_]struct {
-//         arg: usize,
-//         expected: []const u8,
-//     }{
-//         .{ .arg = 0, .expected = "n/a" },
-//         .{ .arg = 2, .expected = "LBRACK" },
-//         .{ .arg = token.token_names.len, .expected = "" },
-//     };
-//
-//     for (test_cases) |tc| {
-//         const token_name = lexer.tokenNameAtIndex(tc.arg);
-//         try testing.expectEqualStrings(tc.expected, token_name);
-//     }
-// }
+    try testing.expectEqualStrings("test", lexer.cursor.input);
+    try testing.expectEqual(0, lexer.cursor.pointer);
+    try testing.expectEqual('t', lexer.cursor.current_char);
+}
 
-// test "expect Lexer detects end-of-line" {
-//     const testing = std.testing;
-//
-//     const test_cases = [_]struct {
-//         arg: u8,
-//         expected: bool = false,
-//     }{
-//         .{ .arg = 't', .expected = true },
-//         .{ .arg = cursor.eof },
-//         .{ .arg = '\n' },
-//         .{ .arg = '\u{ff}' },
-//     };
-//
-//     for (test_cases) |tc| {
-//         const lexer = try Lexer.init(testing.allocator, "test", 0, tc.arg);
-//         defer lexer.deinit();
-//         try testing.expectEqual(tc.expected, lexer.isNotEndOfLine());
-//     }
-// }
+test "expect Lexer returns token names by index" {
+    const testing = std.testing;
+    const lexer = try Lexer.init(testing.allocator, "test", 0, 't');
+    defer lexer.deinit();
 
-// test "expect Lexer can check characters belong to aliases" {
-//     const testing = std.testing;
-//
-//     const test_cases = [_]struct {
-//         arg: u8,
-//         expected: bool = false,
-//     }{
-//         .{ .arg = 't', .expected = true },
-//         .{ .arg = 'T', .expected = true },
-//         .{ .arg = '0', .expected = true },
-//         .{ .arg = '\n' },
-//         .{ .arg = '\u{ff}' },
-//     };
-//
-//     for (test_cases) |tc| {
-//         const lexer = try Lexer.init(testing.allocator, "test", 0, tc.arg);
-//         defer lexer.deinit();
-//         try testing.expectEqual(tc.expected, lexer.isAliasName());
-//     }
-// }
-//
-// test "expect Lexer can check asterisk is for glob aliases" {
-//     const testing = std.testing;
-//
-//     const test_cases = [_]struct {
-//         arg: u8,
-//         expected: bool,
-//     }{
-//         .{ .arg = 't', .expected = false },
-//         .{ .arg = '*', .expected = true },
-//     };
-//
-//     for (test_cases) |tc| {
-//         const lexer = try Lexer.init(testing.allocator, "test", 0, tc.arg);
-//         defer lexer.deinit();
-//         try testing.expectEqual(tc.expected, lexer.isGlob());
-//     }
-// }
-//
-// test "expect Lexer can consume whitespace" {
-//     const testing = std.testing;
-//
-//     const test_cases = [_]struct {
-//         arg: []const u8,
-//         expected: u8,
-//     }{
-//         .{ .arg = "test", .expected = 't' },
-//         .{ .arg = " test", .expected = 't' },
-//         .{ .arg = "   test", .expected = 't' },
-//         .{ .arg = "   ", .expected = cursor.eof },
-//     };
-//
-//     for (test_cases) |tc| {
-//         var lexer = try Lexer.init(testing.allocator, tc.arg, 0, tc.arg[0]);
-//         defer lexer.deinit();
-//         lexer.whitespace();
-//         try testing.expectEqual(tc.expected, lexer.cursor.current_char);
-//     }
-// }
-//
-// test "expect Lexer to consume alias tokens" {
-//     const testing = std.testing;
-//
-//     var lexer = try Lexer.init(testing.allocator, "test", 0, 't');
-//     defer lexer.deinit();
-//
-//     const actual = try lexer.alias();
-//     defer actual.deinit();
-//
-//     try testing.expectEqualStrings("test", actual.text);
-//     try testing.expectEqual(.alias, actual.kind);
-// }
+    const test_cases = [_]struct {
+        arg: usize,
+        expected: []const u8,
+    }{
+        .{ .arg = 0, .expected = "n/a" },
+        .{ .arg = 2, .expected = "LBRACK" },
+        .{ .arg = token.token_names.len, .expected = "" },
+    };
 
-// test "expect Lexer to consume path tokens" {
-//     const testing = std.testing;
-//
-//     const input = "/some/test/path";
-//     var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
-//     defer lexer.deinit();
-//
-//     const actual = try lexer.path();
-//     defer actual.deinit();
-//
-//     try testing.expectEqualStrings("/some/test/path", actual.text);
-//     try testing.expectEqual(token.path, actual.kind);
-// }
-//
-// test "expect Lexer to consume glob tokens" {
-//     const testing = std.testing;
-//
-//     const input = "*";
-//     var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
-//     defer lexer.deinit();
-//
-//     const actual = try lexer.glob();
-//     defer actual.deinit();
-//
-//     try testing.expectEqualStrings("*", actual.text);
-//     try testing.expectEqual(token.TokenKind.glob, actual.kind);
-// }
-//
-// test "expect Lexer to parse valid alias and path tokens" {
-//     const testing = std.testing;
-//
-//     const input =
-//         \\[test]/some/test/path
-//         \\/another/test/path
-//     ;
-//     var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
-//     defer lexer.deinit();
-//
-//     var tokens = ArrayList(token.Token).init(testing.allocator);
-//     defer {
-//         for (tokens.items) |t| t.deinit();
-//         tokens.deinit();
-//     }
-//
-//     while (true) {
-//         const t = try lexer.nextToken();
-//         try tokens.append(t);
-//         if (t.kind == .eof) {
-//             break;
-//         }
-//     }
-//
-//     try testing.expectEqual(6, tokens.items.len);
-//     try testing.expectEqual(.lbrack, tokens.items[0].kind);
-//     try testing.expectEqualStrings("[", tokens.items[0].text);
-//
-//     try testing.expectEqual(.alias, tokens.items[1].kind);
-//     try testing.expectEqualStrings("test", tokens.items[1].text);
-//
-//     try testing.expectEqual(.rbrack, tokens.items[2].kind);
-//     try testing.expectEqualStrings("]", tokens.items[2].text);
-//
-//     try testing.expectEqual(.path, tokens.items[3].kind);
-//     try testing.expectEqualStrings("/some/test/path", tokens.items[3].text);
-//
-//     try testing.expectEqual(.path, tokens.items[4].kind);
-//     try testing.expectEqualStrings("/another/test/path", tokens.items[4].text);
-//
-//     try testing.expectEqual(.eof, tokens.items[5].kind);
-//     try testing.expectEqualStrings("<EOF>", tokens.items[5].text);
-// }
-//
+    for (test_cases) |tc| {
+        const token_name = lexer.tokenNameAtIndex(tc.arg);
+        try testing.expectEqualStrings(tc.expected, token_name);
+    }
+}
+
+test "expect Lexer detects end-of-line" {
+    const testing = std.testing;
+
+    const test_cases = [_]struct {
+        arg: u8,
+        expected: bool = false,
+    }{
+        .{ .arg = 't', .expected = true },
+        .{ .arg = cursor.eof },
+        .{ .arg = '\n' },
+        .{ .arg = '\u{ff}' },
+    };
+
+    for (test_cases) |tc| {
+        const lexer = try Lexer.init(testing.allocator, "test", 0, tc.arg);
+        defer lexer.deinit();
+        try testing.expectEqual(tc.expected, lexer.isNotEndOfLine());
+    }
+}
+
+test "expect Lexer can check characters belong to aliases" {
+    const testing = std.testing;
+
+    const test_cases = [_]struct {
+        arg: u8,
+        expected: bool = false,
+    }{
+        .{ .arg = 't', .expected = true },
+        .{ .arg = 'T', .expected = true },
+        .{ .arg = '0', .expected = true },
+        .{ .arg = '\n' },
+        .{ .arg = '\u{ff}' },
+    };
+
+    for (test_cases) |tc| {
+        const lexer = try Lexer.init(testing.allocator, "test", 0, tc.arg);
+        defer lexer.deinit();
+        try testing.expectEqual(tc.expected, lexer.isAliasName());
+    }
+}
+
+test "expect Lexer can check asterisk is for glob aliases" {
+    const testing = std.testing;
+
+    const test_cases = [_]struct {
+        arg: u8,
+        expected: bool,
+    }{
+        .{ .arg = 't', .expected = false },
+        .{ .arg = '*', .expected = true },
+    };
+
+    for (test_cases) |tc| {
+        const lexer = try Lexer.init(testing.allocator, "test", 0, tc.arg);
+        defer lexer.deinit();
+        try testing.expectEqual(tc.expected, lexer.isGlob());
+    }
+}
+
+test "expect Lexer can consume whitespace" {
+    const testing = std.testing;
+
+    const test_cases = [_]struct {
+        arg: []const u8,
+        expected: u8,
+    }{
+        .{ .arg = "test", .expected = 't' },
+        .{ .arg = " test", .expected = 't' },
+        .{ .arg = "   test", .expected = 't' },
+        .{ .arg = "   ", .expected = cursor.eof },
+    };
+
+    for (test_cases) |tc| {
+        var lexer = try Lexer.init(testing.allocator, tc.arg, 0, tc.arg[0]);
+        defer lexer.deinit();
+        lexer.whitespace();
+        try testing.expectEqual(tc.expected, lexer.cursor.current_char);
+    }
+}
+
+test "expect Lexer to consume alias tokens" {
+    const testing = std.testing;
+
+    var lexer = try Lexer.init(testing.allocator, "test", 0, 't');
+    defer lexer.deinit();
+
+    const actual = try lexer.alias();
+    defer actual.deinit();
+
+    try testing.expectEqualStrings("test", actual.text);
+    try testing.expectEqual(.alias, actual.kind);
+}
+
+test "expect Lexer to consume path tokens" {
+    const testing = std.testing;
+
+    const input = "/some/test/path";
+    var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
+    defer lexer.deinit();
+
+    const actual = try lexer.path();
+    defer actual.deinit();
+
+    try testing.expectEqualStrings("/some/test/path", actual.text);
+    try testing.expectEqual(.path, actual.kind);
+}
+
+test "expect Lexer to consume glob tokens" {
+    const testing = std.testing;
+
+    const input = "*";
+    var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
+    defer lexer.deinit();
+
+    const actual = try lexer.glob();
+    defer actual.deinit();
+
+    try testing.expectEqualStrings("*", actual.text);
+    try testing.expectEqual(token.TokenKind.glob, actual.kind);
+}
+
+test "expect Lexer to parse valid alias and path tokens" {
+    const testing = std.testing;
+
+    const input =
+        \\[test]/some/test/path
+        \\/another/test/path
+    ;
+    var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
+    defer lexer.deinit();
+
+    var tokens = ArrayList(token.Token).init(testing.allocator);
+    defer {
+        for (tokens.items) |t| t.deinit();
+        tokens.deinit();
+    }
+
+    while (true) {
+        const t = try lexer.nextToken();
+        try tokens.append(t);
+        if (t.kind == .eof) {
+            break;
+        }
+    }
+
+    try testing.expectEqual(6, tokens.items.len);
+    try testing.expectEqual(.lbrack, tokens.items[0].kind);
+    try testing.expectEqualStrings("[", tokens.items[0].text);
+
+    try testing.expectEqual(.alias, tokens.items[1].kind);
+    try testing.expectEqualStrings("test", tokens.items[1].text);
+
+    try testing.expectEqual(.rbrack, tokens.items[2].kind);
+    try testing.expectEqualStrings("]", tokens.items[2].text);
+
+    try testing.expectEqual(.path, tokens.items[3].kind);
+    try testing.expectEqualStrings("/some/test/path", tokens.items[3].text);
+
+    try testing.expectEqual(.path, tokens.items[4].kind);
+    try testing.expectEqualStrings("/another/test/path", tokens.items[4].text);
+
+    try testing.expectEqual(.eof, tokens.items[5].kind);
+    try testing.expectEqualStrings("<EOF>", tokens.items[5].text);
+}
+
 test "expect Lexer to parse relative path tokens" {
     const testing = std.testing;
 
@@ -335,7 +335,10 @@ test "expect Lexer to parse relative path tokens" {
     defer lexer.deinit();
 
     var tokens = ArrayList(token.Token).init(testing.allocator);
-    defer tokens.deinit();
+    defer {
+        for(tokens.items) |t| t.deinit();
+        tokens.deinit();
+    }
 
     while (true) {
         const t = try lexer.nextToken();
@@ -356,40 +359,40 @@ test "expect Lexer to parse relative path tokens" {
     try testing.expectEqualStrings(tokens.items[2].text, "<EOF>");
 }
 
-// test "expect Lexer to parse glob token tokens" {
-//     const testing = std.testing;
-//
-//     const input = "[*]/some/test/path";
-//     var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
-//     defer lexer.deinit();
-//
-//     var tokens = ArrayList(token.Token).init(testing.allocator);
-//     defer {
-//         for (tokens.items) |t| t.deinit();
-//         tokens.deinit();
-//     }
-//
-//     while (true) {
-//         const t = try lexer.nextToken();
-//         try tokens.append(t);
-//         if (t.kind == .eof) {
-//             break;
-//         }
-//     }
-//
-//     try testing.expectEqual(5, tokens.items.len);
-//     try testing.expectEqual(tokens.items[0].kind, .lbrack);
-//     try testing.expectEqualStrings("[", tokens.items[0].text);
-//
-//     try testing.expectEqual(tokens.items[1].kind, .glob);
-//     try testing.expectEqualStrings("*", tokens.items[1].text);
-//
-//     try testing.expectEqual(tokens.items[2].kind, .rbrack);
-//     try testing.expectEqualStrings("]", tokens.items[2].text);
-//
-//     try testing.expectEqual(tokens.items[3].kind, .path);
-//     try testing.expectEqualStrings("/some/test/path", tokens.items[3].text);
-//
-//     try testing.expectEqual(tokens.items[4].kind, .eof);
-//     try testing.expectEqualStrings("<EOF>", tokens.items[4].text);
-// }
+test "expect Lexer to parse glob token tokens" {
+    const testing = std.testing;
+
+    const input = "[*]/some/test/path";
+    var lexer = try Lexer.init(testing.allocator, input, 0, input[0]);
+    defer lexer.deinit();
+
+    var tokens = ArrayList(token.Token).init(testing.allocator);
+    defer {
+        for (tokens.items) |t| t.deinit();
+        tokens.deinit();
+    }
+
+    while (true) {
+        const t = try lexer.nextToken();
+        try tokens.append(t);
+        if (t.kind == .eof) {
+            break;
+        }
+    }
+
+    try testing.expectEqual(5, tokens.items.len);
+    try testing.expectEqual(tokens.items[0].kind, .lbrack);
+    try testing.expectEqualStrings("[", tokens.items[0].text);
+
+    try testing.expectEqual(tokens.items[1].kind, .glob);
+    try testing.expectEqualStrings("*", tokens.items[1].text);
+
+    try testing.expectEqual(tokens.items[2].kind, .rbrack);
+    try testing.expectEqualStrings("]", tokens.items[2].text);
+
+    try testing.expectEqual(tokens.items[3].kind, .path);
+    try testing.expectEqualStrings("/some/test/path", tokens.items[3].text);
+
+    try testing.expectEqual(tokens.items[4].kind, .eof);
+    try testing.expectEqualStrings("<EOF>", tokens.items[4].text);
+}
