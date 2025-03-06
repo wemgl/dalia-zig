09512f810da75d329f43fe08bd4c38fee8fdd5ae
wembleyleach <wembley.gl@icloud.com>
2025-03-05 20:02:08 -0500

    Add support for building dalia for non-macOS targets

--- build.zig
+++ build.zig
@@ -2,11 +2,22 @@ const std = @import("std");
 
 const targets: []const std.Target.Query = &.{
     .{ .cpu_arch = .aarch64, .os_tag = .macos },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu },
+    .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .musl },
+    .{ .cpu_arch = .aarch64, .os_tag = .windows },
 };
 
 pub fn build(b: *std.Build) !void {
+    const optimize = b.standardOptimizeOption(.{});
+    const version = b.option(
+        []const u8,
+        "version",
+        "The semantic version of this CLI",
+    ) orelse "2.0.0";
+    const run_step = b.step("run", "Run dalia");
+
     for (targets) |target| {
-        const optimize = b.standardOptimizeOption(.{});
         const exe = b.addExecutable(.{
             .name = "dalia",
             .root_source_file = b.path("src/main.zig"),
@@ -16,14 +27,17 @@ pub fn build(b: *std.Build) !void {
             .strip = true,
         });
 
-        const version = b.option(
-            []const u8,
-            "version",
-            "The semantic version of this CLI",
-        ) orelse "2.0.0";
         const options = b.addOptions();
         options.addOption([]const u8, "version", version);
         exe.root_module.addOptions("config", options);
+
+        if (target.os_tag == .macos) {
+            const run_exe = b.addRunArtifact(exe);
+            run_exe.step.dependOn(b.getInstallStep());
+            if (b.args) |args| run_exe.addArgs(args);
+            run_step.dependOn(&run_exe.step);
+        }
+
         const target_output = b.addInstallArtifact(exe, .{
             .dest_dir = .{
                 .override = .{
@@ -33,12 +47,5 @@ pub fn build(b: *std.Build) !void {
         });
 
         b.getInstallStep().dependOn(&target_output.step);
-
-        const run_exe = b.addRunArtifact(exe);
-        run_exe.step.dependOn(b.getInstallStep());
-        if (b.args) |args| run_exe.addArgs(args);
-
-        const run_step = b.step("run", "Run dalia");
-        run_step.dependOn(&run_exe.step);
     }
 }
