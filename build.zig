4753712fefbcba6a9d1a6a2a7fd1c7f2b81ab006
wembleyleach <wembley.gl@icloud.com>
2025-03-05 19:50:10 -0500

    Update zig build script to create macos aarch64 executable

--- build.zig
+++ build.zig
@@ -1,31 +1,44 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-    const exe = b.addExecutable(.{
-        .name = "dalia",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-        .single_threaded = true,
-        .strip = true,
-    });
+const targets: []const std.Target.Query = &.{
+    .{ .cpu_arch = .aarch64, .os_tag = .macos },
+};
 
-    const version = b.option(
-        []const u8,
-        "version",
-        "The semantic version of this CLI",
-    ) orelse "2.0.0";
-    const options = b.addOptions();
-    options.addOption([]const u8, "version", version);
-    exe.root_module.addOptions("config", options);
-    b.installArtifact(exe);
+pub fn build(b: *std.Build) !void {
+    for (targets) |target| {
+        const optimize = b.standardOptimizeOption(.{});
+        const exe = b.addExecutable(.{
+            .name = "dalia",
+            .root_source_file = b.path("src/main.zig"),
+            .target = b.resolveTargetQuery(target),
+            .optimize = optimize,
+            .single_threaded = true,
+            .strip = true,
+        });
 
-    const run_exe = b.addRunArtifact(exe);
-    run_exe.step.dependOn(b.getInstallStep());
-    if (b.args) |args| run_exe.addArgs(args);
+        const version = b.option(
+            []const u8,
+            "version",
+            "The semantic version of this CLI",
+        ) orelse "2.0.0";
+        const options = b.addOptions();
+        options.addOption([]const u8, "version", version);
+        exe.root_module.addOptions("config", options);
+        const target_output = b.addInstallArtifact(exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try target.zigTriple(b.allocator),
+                },
+            },
+        });
 
-    const run_step = b.step("run", "Run dalia");
-    run_step.dependOn(&run_exe.step);
+        b.getInstallStep().dependOn(&target_output.step);
+
+        const run_exe = b.addRunArtifact(exe);
+        run_exe.step.dependOn(b.getInstallStep());
+        if (b.args) |args| run_exe.addArgs(args);
+
+        const run_step = b.step("run", "Run dalia");
+        run_step.dependOn(&run_exe.step);
+    }
 }
